75032932ffe690c0670a278c1277f890e6b3e1c5
mahfuj890 <mahfujali490@gmail.com>
2023-05-08 18:11:01 +0600

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import "./App.css";
-import { Button } from '@chakra-ui/react'
-
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <>
-<Button colorScheme='blue'>Button</Button>
-  </>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "250px 1fr",
+      }}
+    >
+      <GridItem area="nav" bg={"gold"}>
+        Navbar
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" paddingX={5}>
+          sidebar
+        </GridItem>
+      </Show>
+      <GridItem area="main">
+        main
+        {/* <Box paddingLeft={2}>
+      <GameHeading gameQuery={gameQuery} />
+      <Flex marginBottom={5}>
+        <Box marginRight={5}>
+          <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform}) } />
+        </Box>
+        <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })} />
+      </Flex>
+    </Box> */}
+        {/* <GameGrid gameQuery={gameQuery} /> */}
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
